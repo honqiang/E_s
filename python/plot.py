393b4b20621f64cb6e7506e23457d1e2fd6ab915
honqiang.lee <honqiang.lee@gmail.com>
2018-08-18 17:15:19 +0800

    2018年8月18日

--- python/plot.py
+++ python/plot.py
@@ -11,19 +11,18 @@ a = np.loadtxt('e:/课题/result/Rs.txt')
 ncolors = 256
 color_array = plt.get_cmap('gist_rainbow_r')(range(ncolors))
 # change alpha values
-color_array[:,-1] = np.linspace(0.0,1.0,ncolors)
-
+color_array[:, -1] = np.linspace(0.0, 1.0, ncolors)
 # create a colormap object
-map_object = LinearSegmentedColormap.from_list(name='rainbow_alpha',colors=color_array)
-
+map_object = LinearSegmentedColormap.from_list(
+    name='rainbow_alpha', colors=color_array)
 # register this new colormap with matplotlib
 plt.register_cmap(cmap=map_object)
-
 # show some example data
-f,ax = plt.subplots()
-h = ax.imshow(a,cmap='rainbow_alpha')
-plt.colorbar(mappable=h)
+f, ax = plt.subplots()
+h = ax.imshow(a, cmap='rainbow_alpha')
+# plt.colorbar(mappable=h)
+
+plt.axis('off')
+plt.savefig("e:/课题/result/Result.png", transparent=True,
+            bbox_inches='tight', dpi=1000)
 plt.show()
-
-
-
